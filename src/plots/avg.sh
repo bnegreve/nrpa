5374275d85ac21bc68468a3c3b7c76f2ac447d2e
Benjamin Negrevergne <benjamin.negrevergne@lamsade.dauphine.fr>
2017-05-13 15:59:19 +0200

    avg.sh now outputs ymax and y 95%ci

--- src/plots/avg.sh
+++ src/plots/avg.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-awk 'BEGIN  { xmax = 0; }                                                               \
+awk 'BEGIN  { xmax = 0; ymax = 0; }                                                     \
      /^[^#]/{ count[$2]++; xsum[$2]+=$3; sum[$2]+=$4; sqsum[$2]+=$4*$4;                 \
               if($3 > xmax) { xmax=$3; };                                               \
+              if($4 > ymax) { ymax=$4; };                                               \
      }                                                                                  \
      END    {                                                                    	\
+              print 0, 0, 0, 0, 0, 0, 0;                                                \
               for( i in count){					                        \
+                xavg=xsum[i] / count[i];                                                \
+                yavg=sum[i] / count[i]; 						\
+                ystdev=sqrt( (1/count[i]) * (sqsum[i] - (sum[i]*sum[i]/count[i]))); 	\
+                yci= 1.96 * (ystdev / sqrt(count[i])); 					\
           	print i,								\
-          	    xsum[i] / count[i],							\
-          	    sum[i] / count[i],							\
-          	    sqrt( (1/count[i]) * (sqsum[i] - (sum[i]*sum[i]/count[i]))),        \
-          	    xmax;                                                               \
+          	    xavg,								\
+          	    yavg,								\
+          	    ystdev,        							\
+          	    xmax,          							\
+          	    ymax,                                                               \
+                    yci                                                                 \
        	     }                                                                          \
      }' $*
